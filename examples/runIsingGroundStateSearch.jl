fd4093b81d79227e2ecacec6b017f54c516e0c2e
Stefan Kuehn <skuehn.phys@gmail.com>
2021-11-14 12:37:02 +0200

    Avoid running the example in a global scope.

--- examples/runIsingGroundStateSearch.jl
+++ examples/runIsingGroundStateSearch.jl
@@ -3,32 +3,36 @@
 ############################################################################
 using MatrixProductStates
 
-# Number of Spins
-N = 40
-# Bond Dimension
-D = 20
-# Physical dimension
-d = 2
-# Desired Accuracy
-acc = 1E-10
-# Maximum number of sweeps
-max_sweeps = 10
+let 
+    # Number of Spins
+    N = 40
+    # Bond Dimension
+    D = 20
+    # Physical dimension
+    d = 2
+    # Desired Accuracy
+    acc = 1E-10
+    # Maximum number of sweeps
+    max_sweeps = 10
 
-# Coupling between nearest neigbours
-J = 1.0
-# Coupling to external field
-lambda = 1.0
-# Generate Ising Hamiltonian
-H = getIsingMPO(N, J, lambda)
-# Get the MPO for the total spin
-Sz = getTotalSpinMPO(N)
+    # Coupling between nearest neigbours
+    J = 1.0
+    # Coupling to external field
+    lambda = 1.0
+    # Generate Ising Hamiltonian
+    H = getIsingMPO(N, J, lambda)
+    # Get the MPO for the total spin
+    Sz = getTotalSpinMPO(N)
 
-# Run the variational ground state search
-E0, mps, num_of_sweeps = find_groundstate(H, D, d, acc, max_sweeps)
+    # Run the variational ground state search
+    E0, mps, num_of_sweeps = find_groundstate(H, D, d, acc, max_sweeps)
 
-# Print the results
-println("Groundstate energy:            ", E0)
-println("Groundstate energy density:    ", E0 / N)
-println("Number of sweeps:              ", num_of_sweeps)
-println("Total spin:                    ", expectation_value(mps, Sz))
-println("Entropy along center bond      ", compute_entropy(mps))
+    # Print the results
+    println("Groundstate energy:            ", E0)
+    println("Groundstate energy density:    ", E0 / N)
+    println("Number of sweeps:              ", num_of_sweeps)
+    println("Total spin:                    ", expectation_value(mps, Sz))
+    println("Entropy along center bond      ", compute_entropy(mps))
+
+    nothing
+end
